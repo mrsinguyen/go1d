2398166cbc8ee19ac7b7c6d1ad10270040da90cf
salvatore.leotta@go1.com <salvatore.leotta@go1.com>
2018-09-13 08:55:13 +0800

    Fixing pixo template

--- pixo-template.js
+++ pixo-template.js
@@ -3,29 +3,23 @@ module.exports = ({
     name,
     viewBox,
     pathData
-  }) => `import * as React from 'react'
-  
-  interface Props {
-      size: number;
-      color: string;
-  }
+  }) => `import * as React from "react";
 
-  const ${name}:React.SFC<Props> = ({
-    size = 24,
-    color = "currentcolor",
-    ...props
-  }: Props) => (
-    <svg
-      {...props}
-      viewBox='${viewBox}'
-      width={size}
-      height={size}
-      fill={color}
-    >
-      <path d='${pathData}' />
-    </svg>
-  )
-  
-  ${name}.displayName = "${name}"
+interface Props {
+  size: number;
+  color: string;
+}
 
-  export default ${name}`
+const ${name}: React.SFC<Props> = ({
+  size = 24,
+  color = "currentcolor",
+  ...props
+}: Props) => (
+  <svg {...props} viewBox="${viewBox}" width={size} height={size} fill={color}>
+    <path d="${pathData}" />
+  </svg>
+);
+
+${name}.displayName = "${name}";
+
+export default ${name};`
--- readme.md
+++ readme.md
@@ -20,3 +20,6 @@ yarn docs:dev
 
 ## CONTRIBUTE
 Before any pull requests ensure all changed files are run though tslint which should automaticly correct most code styling errors
+
+## Adding svg icons
+Add the svg to src/icons and then run `yarn pixo`. Make sure to include any created files in the commit (although this handled by a precommit hook, check to be safe).
