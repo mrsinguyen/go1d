3d6ea88839f0522e81651788b03030671ab30aef
Luke Brooker <mail@lukebrooker.com>
2018-09-17 23:32:28 +1000

    Cleanup import/export

--- docs/_src/components/index.js
+++ docs/_src/components/index.js
@@ -1,6 +1,4 @@
-import AllIcons from "./AllIcons";
-
 export { Color } from "./Color";
 export { Colors } from "./Colors";
 export { ComponentDoc } from "./ComponentDoc";
-export { AllIcons };
+export { default as AllIcons } from "./AllIcons";
