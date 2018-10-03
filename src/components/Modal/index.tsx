6ef6792b0319faf5aabbc555e701fdb4f72d117b
Cian O'Leary <cian.oleary@go1.com>
2018-10-03 11:06:29 +1000

    Move to Parent Controlled Visibility and Implement Portal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import { createPortal } from "react-dom";
+import { Transition } from "react-transition-group";
 
+import Button from "../Button";
 import Icon from "../Icon";
 import Text from "../Text";
 import Theme from "../Theme";
@@ -18,54 +21,55 @@ const AbsoluteStyling = {
   margin: "0 auto",
 };
 
+const Unique = () =>
+  Math.random()
+    .toString(36)
+    .substring(2) + new Date().getTime().toString(36);
+
 class Modal extends React.Component<Props, any> {
+  private portalNode: HTMLElement;
+
   constructor(props) {
     super(props);
-    this.state = {
-      open: props.initOpen || false,
-    };
+    this.portalNode = document.createElement("div");
+    this.portalNode.setAttribute("data-portal-id", Unique());
+    document.body.appendChild(this.portalNode);
+  }
 
-    if (props.disableKeyBindings !== true) {
+  public componentDidMount() {
+    if (this.props.onRequestClose && this.props.disableKeyBindings !== true) {
       document.addEventListener("keydown", this.detectEscape);
     }
   }
 
-  public componentDidMount() {
-    if (Object.prototype.hasOwnProperty.call(this.props, "onRef")) {
-      this.props.onRef(this);
-    }
-  }
-
   public componentWillUnmount() {
-    if (Object.prototype.hasOwnProperty.call(this.props, "onRef")) {
-      this.props.onRef(undefined);
-    }
+    document.body.removeChild(this.portalNode);
+    document.removeEventListener("keydown", this.detectEscape);
   }
-
-  public closeModal = () =>
-    this.setState({
-      open: false,
-    });
 
   public detectEscape = event => {
-    if (this.state.open) {
+    if (this.props.isOpen) {
       if (event.keyCode === 27) {
-        this.closeModal();
+        this.props.onRequestClose();
       }
     }
   };
 
-  public openModal = () =>
-    this.setState({
-      open: true,
-    });
+  public handleBackgroundClick = () => {
+    if (
+      this.props.onRequestClose &&
+      this.props.disableBackgroundClose !== true
+    ) {
+      this.props.onRequestClose();
+    }
+  };
 
   public render() {
-    if (this.state.open) {
-      return (
-        <Theme.Consumer>
-          {({ colors }) => {
-            return (
+    const RenderModal = () => {
+      if (this.props.isOpen) {
+        return (
+          <Theme.Consumer>
+            {({ colors }) => (
               <React.Fragment>
                 <View
                   backgroundColor="default"
@@ -77,6 +81,7 @@ class Modal extends React.Component<Props, any> {
                     minHeight: "100vh",
                     width: "100%",
                   }}
+                  onClick={this.handleBackgroundClick}
                 />
                 <View
                   maxWidth={600}
@@ -98,35 +103,36 @@ class Modal extends React.Component<Props, any> {
                       position: "relative",
                     }}
                   >
-                    <Icon
-                      onClick={this.closeModal}
-                      name="Cross"
-                      size={3}
-                      css={{
-                        ...AbsoluteStyling,
-                        right: "initial",
-                        left: 15,
-                        margin: "auto 0",
-                        bottom: 0,
-                        cursor: "pointer",
-                      }}
-                    />
+                    {this.props.onRequestClose && (
+                      <Button
+                        css={{
+                          ...AbsoluteStyling,
+                          right: "initial",
+                          left: 15,
+                          margin: "auto 0",
+                          bottom: 0,
+                          cursor: "pointer",
+                        }}
+                        onClick={this.props.onRequestClose}
+                      >
+                        <Icon name="Cross" size={3} />
+                      </Button>
+                    )}
                     <Text element="h1" textAlign="center" fontSize={3}>
                       {this.props.title}
                     </Text>
                   </View>
-                  <View padding={5}>
-                    {this.props.children && this.props.children(this)}
-                  </View>
+                  <View padding={5}>{this.props.children}</View>
                 </View>
               </React.Fragment>
-            );
-          }}
-        </Theme.Consumer>
-      );
-    }
+            )}
+          </Theme.Consumer>
+        );
+      }
+      return null;
+    };
 
-    return null;
+    return createPortal(RenderModal(), this.portalNode);
   }
 }
 
