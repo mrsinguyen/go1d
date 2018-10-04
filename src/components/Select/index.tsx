cf7e886b100889ef3dcef334e6483dada2a1642f
Cian O'Leary <cian.oleary@go1.com>
2018-10-04 14:32:42 +1000

    Modal Component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -10,11 +10,16 @@ interface Props extends ViewProps {
   options?: Array<{ value: string; label: string }>;
 }
 
+type FocusDirection = "up" | "down" | "first" | "last" | "open";
+
 class Select extends React.Component<Props, any> {
   public state = {
+    closeOverride: false,
     value: null,
     label: null,
     isFocused: false,
+    focusedOptionIndex: null,
+    focusedValue: null,
   };
 
   public inputRef: HTMLElement = null;
@@ -23,6 +28,7 @@ class Select extends React.Component<Props, any> {
     this.setState({
       value,
       label,
+      closeOverride: true,
     });
 
     if (this.props.onChange) {
@@ -37,6 +43,7 @@ class Select extends React.Component<Props, any> {
     if (!this.inputRef) {
       return;
     }
+
     this.inputRef.focus();
   }
 
@@ -44,17 +51,22 @@ class Select extends React.Component<Props, any> {
     if (!this.inputRef) {
       return;
     }
+
     this.inputRef.blur();
   }
 
   public handleOnBlur = () =>
     this.setState({
       isFocused: false,
+      closeOverride: false,
+      focusedOptionIndex: null,
+      focusedValue: null,
     });
 
   public handleOnFocus = () =>
     this.setState({
       isFocused: true,
+      closeOverride: false,
     });
 
   public getInputRef = (ref: HTMLElement) => {
@@ -69,9 +81,107 @@ class Select extends React.Component<Props, any> {
   };
 
   // Keyboard Handler
+  public focusOption(direction: FocusDirection = "first") {
+    const { options } = this.props;
+    let { focusedOptionIndex } = this.state;
+
+    if (direction === "open") {
+      this.setState({
+        focusedOptionIndex,
+        focusedValue: options[focusedOptionIndex],
+        closeOverride: false,
+      });
+      return;
+    }
+
+    if (!options.length) {
+      return;
+    }
+    let nextFocus = 0; // handles 'first'
+
+    if (focusedOptionIndex === null) {
+      focusedOptionIndex = -1;
+    }
+
+    if (direction === "up") {
+      nextFocus =
+        focusedOptionIndex > 0 ? focusedOptionIndex - 1 : options.length - 1;
+    } else if (direction === "down") {
+      nextFocus = (focusedOptionIndex + 1) % options.length;
+    } else if (direction === "last") {
+      nextFocus = options.length - 1;
+    }
+
+    this.setState({
+      focusedOptionIndex: nextFocus,
+      focusedValue: options[nextFocus],
+      closeOverride: false,
+    });
+  }
+
+  public selectOption = Selected => {
+    if (Selected.label && Selected.value) {
+      this.handleValueSelect(Selected.label, Selected.value)();
+    }
+  };
+
+  public onKeyDown = (event: KeyboardEvent) => {
+    const { isDisabled } = this.props;
+
+    const { focusedValue, closeOverride } = this.state;
+
+    if (isDisabled) {
+      return;
+    }
+
+    switch (event.key) {
+      case "Tab":
+        if (event.shiftKey || !focusedValue) {
+          return;
+        }
+        this.selectOption(focusedValue);
+        break;
+      case "Enter":
+        if (focusedValue) {
+          this.selectOption(focusedValue);
+        }
+        break;
+      case "Escape":
+        this.blurInput();
+        break;
+      case " ": // space
+        if (focusedValue) {
+          this.selectOption(focusedValue);
+        }
+        break;
+      case "ArrowUp":
+        if (closeOverride) {
+          this.focusOption("open");
+          break;
+        }
+        this.focusOption("up");
+        break;
+      case "ArrowDown":
+        if (closeOverride) {
+          this.focusOption("open");
+          break;
+        }
+        this.focusOption("down");
+        break;
+      case "Home":
+        this.focusOption("first");
+        break;
+      case "End":
+        this.focusOption("last");
+        break;
+      default:
+        return;
+    }
+    event.preventDefault();
+  };
 
   public render() {
-    const { options, name, onChange, children, ...props } = this.props;
+    const { options, name, onChange, css, children, ...props } = this.props;
 
     return (
       <Theme.Consumer>
@@ -83,7 +193,15 @@ class Select extends React.Component<Props, any> {
               boxShadow={this.state.isFocused ? "strong" : "soft"}
               backgroundColor="background"
               onClick={this.onMenuMouseDown}
+              onKeyDown={this.onKeyDown}
               {...props}
+              css={{
+                border: "1px solid",
+                borderColor: this.state.isFocused
+                  ? colors.accent
+                  : "transparent",
+                ...((css as object) || {}),
+              }}
             >
               <View
                 paddingY={4}
@@ -106,7 +224,7 @@ class Select extends React.Component<Props, any> {
                   alignItems: "center",
                   justifyContent: "center",
                 }}
-                height="100%"
+                height="calc(100% - 2px)"
                 paddingX={3}
                 backgroundColor="background"
               >
@@ -125,7 +243,10 @@ class Select extends React.Component<Props, any> {
               css={{
                 position: "absolute",
                 top: `calc(100% + ${spacing[2]}px)`,
-                display: this.state.isFocused ? "block" : "none",
+                display:
+                  this.state.isFocused === false || this.state.closeOverride
+                    ? "none"
+                    : "block",
               }}
               borderRadius={2}
               width="100%"
@@ -148,6 +269,11 @@ class Select extends React.Component<Props, any> {
                       background: colors.highlight,
                       cursor: "pointer",
                     },
+                    ...(Index === this.state.focusedOptionIndex
+                      ? {
+                          background: colors.highlight,
+                        }
+                      : {}),
                   }}
                 >
                   <Text>{Option.label}</Text>
