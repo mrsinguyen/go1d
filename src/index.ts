3c7f97c52d9011c20c873122e7cdb81b0141b4c9
Shangzhi Pan <shangzhi.pan@go1.com>
2018-10-11 12:52:19 +1000

    Removed TextInput Component since it has been developed by FT6

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ export { DarkMode } from "./components/Theme";
 export { default as Provider } from "./components/Provider";
 export { default as Link } from "./components/Link";
 export { default as EmptyState } from "./components/EmptyState";
-export { default as TextInput } from "./components/TextInput";
 export { default as SearchInput } from "./components/SearchInput";
 
 export {
