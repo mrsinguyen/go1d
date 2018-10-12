196891f011126ef411daa857f2160e0ddfe17782
Shangzhi Pan <shangzhi.pan@go1.com>
2018-10-12 11:37:16 +1000

    Lesson Component, added utils/durationFormater.ts, updated CourseSlat to be more consistent with the rest of the system

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export { DarkMode } from "./components/Theme";
 export { default as Provider } from "./components/Provider";
 export { default as Link } from "./components/Link";
 export { default as EmptyState } from "./components/EmptyState";
+export { default as Lesson } from "./components/Lesson";
 export {
   Notifications,
   NotificationContainer,
