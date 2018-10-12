7ddcc90de8b0ca230aa4bb2784698a0a44752375
Sam Wolski <sam.wolski@go1.com>
2018-10-12 14:53:30 +1000

    Changes as per MR feedback.

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { brandGreys, tint } from "../../foundations";
 import ButtonMinimal from "../ButtonFilled";
-import Container from "../Container";
 import Icon from "../Icon";
 import Theme from "../Theme";
 import View, { Props as ViewProps } from "../View";
@@ -24,53 +23,50 @@ const Banner: React.SFC<Props> = ({
   const iconType = type.charAt(0).toUpperCase() + type.slice(1);
   const tintLevel = type === "success" ? 0.1 : type === "warning" ? 0.2 : 0.2;
   return (
-    <Container>
-      <Theme.Consumer>
-        {({ colors }) => (
+    <Theme.Consumer>
+      {({ colors }) => (
+        <View
+          padding={4}
+          marginY={3}
+          borderRadius={2}
+          boxShadow={floating && "distant"}
+          justifyContent="space-between"
+          data-testid="banner"
+          css={{
+            backgroundColor: `${
+              floating ? brandGreys.lightest : tint(colors[type], tintLevel)
+            }`,
+            borderLeft: `4px solid ${colors[type]}`,
+            flexDirection: "row",
+            justifyContent: "justify",
+            transition: "all 0.2s linear",
+          }}
+          {...props}
+        >
+          <Icon name={iconType} color={type} marginTop={1} />
           <View
-            padding={4}
-            marginTop={3}
-            marginBottom={3}
-            borderRadius={2}
-            boxShadow={floating && "distant"}
-            justifyContent="space-between"
-            data-testid="banner"
-            css={{
-              backgroundColor: `${
-                floating ? brandGreys.lightest : tint(colors[type], tintLevel)
-              }`,
-              borderLeft: `4px solid ${colors[type]}`,
-              flexDirection: "row",
-              justifyContent: "justify",
-              transition: "all 0.2s linear",
-            }}
+            flexWrap="wrap"
+            flexGrow={1}
+            paddingX={5}
+            alignItems={floating ? "center" : "left"}
           >
-            <Icon name={iconType} color={type} marginTop={1} />
-            <View
-              flexWrap="wrap"
-              flexGrow={1}
-              paddingLeft={5}
-              paddingRight={5}
-              alignItems={floating ? "center" : "left"}
-            >
-              {children}
-            </View>
-            {close && (
-              <ButtonMinimal
-                borderRadius={10}
-                boxShadow="none"
-                size="sm"
-                height="1.05rem"
-                onClick={close}
-                data-testid="close"
-              >
-                <Icon name="Cross" color={type} />
-              </ButtonMinimal>
-            )}
+            {children}
           </View>
-        )}
-      </Theme.Consumer>
-    </Container>
+          {close && (
+            <ButtonMinimal
+              borderRadius={10}
+              boxShadow="none"
+              size="sm"
+              height="1.05rem"
+              onClick={close}
+              data-testid="close"
+            >
+              <Icon name="Cross" color={type} />
+            </ButtonMinimal>
+          )}
+        </View>
+      )}
+    </Theme.Consumer>
   );
 };
 
