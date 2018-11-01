d2f02578e37bd522e0effe2c7cae713a02d7442b
Cian O'Leary <cian.oleary@go1.com>
2018-11-01 10:20:56 +1000

    Carousel Component

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,307 @@
+import * as TWEEN from "@tweenjs/tween.js";
+import * as React from "react";
+import ContainerDimensions from "react-container-dimensions";
+
+import View, { Props as ViewProps } from "../View";
+
+import foundations from "../../foundations";
+import ButtonFilled from "../ButtonFilled";
+
+interface StandardProps extends ViewProps {
+  children?: React.ReactNode;
+  slidesToShow?: number;
+  gutter?: number;
+  clickScrollAmount?: number; // Number of slides that move on next click
+  slideAnimationDuration?: 300;
+}
+
+interface BreakpointProps {
+  sm?: StandardProps;
+  md?: StandardProps;
+  lg?: StandardProps;
+}
+
+interface Props extends StandardProps {
+  Breakpoints?: BreakpointProps;
+}
+
+class Carousel extends React.Component<Props, any> {
+  public static defaultProps = {
+    slidesToShow: 1,
+    gutter: 2,
+    clickScrollAmount: 1,
+  };
+  public timer = null;
+  public slideRefs = [];
+  public sliderContainerRef = React.createRef();
+
+  public state = {
+    currentSlide: 0,
+    finishedScrolling: true,
+  };
+
+  public componentDidMount() {
+    if (this.sliderContainerRef) {
+      const Element: any = this.sliderContainerRef.current;
+      Element.addEventListener("scroll", this.handleScrollTimer, false);
+    }
+  }
+
+  public componentDidUpdate() {
+    const { gutter, children } = this.props;
+    const { finishedScrolling, currentSlide } = this.state;
+    const Slider: any = this.sliderContainerRef.current;
+    const SliderScroll = Slider.scrollLeft + foundations.spacing[gutter];
+    const LastSlideCurrent = this.slideRefs.slice(-1)[0].current;
+    const LastSlideRightEdge =
+      LastSlideCurrent.offsetLeft + LastSlideCurrent.offsetWidth;
+
+    if (LastSlideRightEdge <= SliderScroll + Slider.offsetWidth) {
+      // Cannot scroll further right
+      if (finishedScrolling === false) {
+        this.setState({
+          finishedScrolling: true,
+        });
+      }
+    } else {
+      if (finishedScrolling) {
+        this.setState({
+          finishedScrolling: false,
+        });
+      }
+    }
+
+    if (React.Children.toArray(children).length <= currentSlide) {
+      // Incase some caroursel children update
+      this.setState({
+        currentSlide: React.Children.toArray(children).length - 1,
+      });
+    }
+  }
+
+  public componentWillUnmount() {
+    if (this.sliderContainerRef) {
+      const Element: any = this.sliderContainerRef.current;
+      Element.removeEventListener("scroll", this.handleScrollTimer, false);
+    }
+  }
+
+  public handleScrollTimer = () => {
+    if (this.timer !== null) {
+      clearTimeout(this.timer);
+    }
+
+    this.timer = setTimeout(this.handleScrollFinished, 150);
+  };
+
+  public handleScrollFinished = () => {
+    if (this.slideRefs.length > 0) {
+      // Set the CurrentSlide when the user is finished scrolling
+      const { gutter } = this.props;
+
+      const Slider: any = this.sliderContainerRef.current;
+      const SliderScroll = Slider.scrollLeft + foundations.spacing[gutter];
+
+      const Selected = this.slideRefs.findIndex((Ref, Index) => {
+        // WARNING - INDEXED FROM 1
+        const CurrentRef = Ref.current;
+
+        if (
+          SliderScroll <= CurrentRef.offsetLeft &&
+          SliderScroll < SliderScroll + CurrentRef.offsetWidth
+        ) {
+          return true;
+        }
+
+        return false;
+      });
+
+      let FinishedScrolling = false;
+
+      const SliderRightEdge = SliderScroll + Slider.offsetWidth;
+      const LastSlide: any = this.slideRefs.slice(-1)[0];
+      const LastSlideCurrent = LastSlide.current;
+      const LastSlideRightEdge =
+        LastSlideCurrent.offsetLeft + LastSlideCurrent.offsetWidth;
+      if (LastSlideRightEdge <= SliderRightEdge) {
+        // Cannot scroll further right
+        FinishedScrolling = true;
+      }
+
+      const CurrentSlide = this.slideRefs[Selected - 1].current;
+
+      if (CurrentSlide.offsetLeft < SliderScroll && !FinishedScrolling) {
+        // Snap to a start position
+        if (
+          CurrentSlide.offsetLeft + CurrentSlide.offsetWidth / 2 <
+          SliderScroll
+        ) {
+          this.scrollToIndex(Selected)();
+        } else {
+          this.scrollToIndex(Selected - 1)();
+        }
+      }
+
+      this.setState({
+        currentSlide: Selected - 1,
+        finishedScrolling: FinishedScrolling,
+      });
+    }
+  };
+
+  public scrollToIndex = Index => () => {
+    const { children } = this.props;
+    let ScrollIndex = Index;
+
+    if (ScrollIndex < 0) {
+      ScrollIndex = 0;
+    }
+
+    if (ScrollIndex >= React.Children.toArray(children).length - 1) {
+      ScrollIndex = React.Children.toArray(children).length - 1;
+    }
+
+    if (ScrollIndex < React.Children.toArray(children).length) {
+      const Slider: any = this.sliderContainerRef.current;
+      const ElementOffset = this.slideRefs[ScrollIndex].current.offsetLeft;
+
+      const coords = { x: Slider.scrollLeft, y: 0 }; // Start at (0, 0)
+      let allowAnimationPropogation = true;
+      const animate = time => {
+        if (allowAnimationPropogation) {
+          requestAnimationFrame(animate);
+        }
+        TWEEN.update(time);
+      };
+
+      requestAnimationFrame(animate);
+
+      new TWEEN.Tween(coords)
+        .to({ x: ElementOffset, y: 0 }, 300)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .onUpdate(() => {
+          Slider.scrollTo(coords.x, 0);
+        })
+        .onComplete(() => {
+          allowAnimationPropogation = false;
+        })
+        .start();
+
+      this.setState(prevState => ({
+        currentSlide: prevState.currentSlide + 1,
+      }));
+    }
+  };
+
+  public render() {
+    const {
+      children,
+      slidesToShow,
+      gutter,
+      css,
+      clickScrollAmount,
+    } = this.props;
+    const { currentSlide, finishedScrolling } = this.state;
+
+    return (
+      <View position="relative">
+        <View
+          innerRef={this.sliderContainerRef}
+          flexDirection="row"
+          width="100%"
+          css={{
+            overflowX: "auto",
+            "::-webkit-scrollbar": {
+              width: 0,
+              background: "transparent",
+            },
+            ...(css as object),
+          }}
+        >
+          {React.Children.map(children, (Slide, Index) => {
+            const SlideRef = React.createRef();
+            this.slideRefs[Index] = SlideRef;
+
+            return (
+              <View
+                innerRef={SlideRef}
+                maxWidth={`${100 / slidesToShow}%`}
+                paddingX={foundations.spacing[gutter]}
+                css={{
+                  ":first-child": {
+                    paddingLeft: 0,
+                  },
+                  ":last-child": {
+                    paddingRight: 0,
+                  },
+                }}
+              >
+                {Slide}
+              </View>
+            );
+          })}
+        </View>
+        {currentSlide > 0 && (
+          <ButtonFilled
+            onClick={this.scrollToIndex(
+              this.state.currentSlide - clickScrollAmount
+            )}
+            iconName="ChevronLeft"
+            position="absolute"
+            height={32}
+            width={32}
+            justifyContent="center"
+            css={{
+              borderRadius: "50%",
+              top: "calc(50% - 16px)",
+              left: -16,
+            }}
+          />
+        )}
+        {!finishedScrolling &&
+          currentSlide < this.slideRefs.length - 1 && (
+            <ButtonFilled
+              onClick={this.scrollToIndex(
+                this.state.currentSlide + clickScrollAmount
+              )}
+              iconName="ChevronRight"
+              position="absolute"
+              height={32}
+              width={32}
+              justifyContent="center"
+              css={{
+                borderRadius: "50%",
+                top: "calc(50% - 16px)",
+                right: -16,
+              }}
+            />
+          )}
+      </View>
+    );
+  }
+}
+
+export default props => (
+  <ContainerDimensions>
+    {Params => {
+      let size = "lg";
+      if (Params.width < 600) {
+        size = "sm";
+      }
+
+      if (Params.width > 600 && Params.width < 1100) {
+        size = "md";
+      }
+
+      const { breakpoints = {}, ...RemainingProps } = props;
+
+      const PassProps = {
+        // Use the props for the current size
+        ...RemainingProps,
+        ...(breakpoints[size] || {}),
+      };
+
+      return <Carousel size={size} {...PassProps} />;
+    }}
+  </ContainerDimensions>
+);
