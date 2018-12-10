8e2c353d83d9b9a6d372570ae6eb2332b077caea
Cian O'Leary <cian.oleary@go1.com>
2018-12-10 14:39:30 +1000

    CTA Card Responsive Fixes

--- src/components/CTACard/index.tsx
+++ src/components/CTACard/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Prose from "../Prose";
 import Text from "../Text";
+import Theme from "../Theme";
 import View, { ViewProps } from "../View";
 
 export interface CTACardProps extends ViewProps {
@@ -22,63 +23,76 @@ const CTACard: React.SFC<CTACardProps> = ({
   backgroundColor = "background",
   ...props
 }: CTACardProps) => (
-  <View
-    boxShadow="soft"
-    borderRadius={3}
-    backgroundColor={backgroundColor}
-    maxWidth={385}
-    width="100%"
-    flexDirection="column"
-    {...props}
-  >
-    <View
-      paddingY={6}
-      paddingX={3}
-      alignItems="center"
-      justifyContent="center"
-      minHeight={160}
-    >
-      {iconImage && (
-        <View paddingBottom={5} element="img" width={60} src={iconImage} />
-      )}
-      {title && (
-        <Text
-          fontSize={4}
+  <Theme.Consumer>
+    {({ breakpoints, spacing }) => (
+      <View
+        boxShadow="soft"
+        borderRadius={3}
+        backgroundColor={backgroundColor}
+        maxWidth={385}
+        width="100%"
+        flexDirection="column"
+        {...props}
+      >
+        <View
+          paddingY={6}
+          paddingX={3}
+          alignItems="center"
+          justifyContent="center"
+          minHeight={160}
+        >
+          {iconImage && (
+            <View paddingBottom={5} element="img" width={60} src={iconImage} />
+          )}
+          {title && (
+            <Text
+              fontSize={4}
+              css={{
+                textAlign: "center",
+              }}
+            >
+              {title}
+            </Text>
+          )}
+        </View>
+        {subtitle && (
+          <View borderColor="faded" borderTop={1} paddingY={5}>
+            <Text
+              fontSize={4}
+              css={{
+                textAlign: "center",
+              }}
+            >
+              {subtitle}
+            </Text>
+          </View>
+        )}
+        <View
+          paddingTop={7}
+          paddingX={6}
+          minHeight={90}
+          flexGrow={1}
+          borderColor="faded"
+          borderTop={1}
+        >
+          {description && <Prose HTML={description} />}
+          {children}
+        </View>
+        <View
+          paddingY={6}
+          paddingX={7}
           css={{
-            textAlign: "center",
+            [breakpoints.sm]: {
+              paddingLeft: spacing[6],
+              paddingRight: spacing[6],
+            },
           }}
         >
-          {title}
-        </Text>
-      )}
-    </View>
-    {subtitle && (
-      <View borderColor="faded" borderTop={1} paddingY={5}>
-        <Text
-          fontSize={4}
-          css={{
-            textAlign: "center",
-          }}
-        >
-          {subtitle}
-        </Text>
+          {button}
+        </View>
       </View>
     )}
-    <View
-      paddingTop={7}
-      paddingX={6}
-      minHeight={90}
-      flexGrow={1}
-      borderColor="faded"
-      borderTop={1}
-    >
-      {description && <Prose HTML={description} />}
-      {children}
-    </View>
-    <View paddingY={6} paddingX={7}>
-      {button}
-    </View>
-  </View>
+  </Theme.Consumer>
 );
 
 export default CTACard;
