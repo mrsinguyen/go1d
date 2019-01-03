b781ee54615d05d7507987b4179e69ae3947deb1
Greg Horvath <greg.horvath@go1.com>
2019-01-03 15:14:52 +1000

    Accommodate for decimals where appropriate

--- src/utils/priceFormatter.test.ts
+++ src/utils/priceFormatter.test.ts
@@ -6,4 +6,9 @@ describe("Price formatter", () => {
     expect(formatPrice("GBP", 20)).toEqual("£20");
     expect(formatPrice("EUR", 2000)).toEqual("€2,000");
   });
+
+  it("keeps decimals where appropriate", () => {
+    expect(formatPrice("EUR", 15.99)).toEqual("€15.99");
+    expect(formatPrice("EUR", 4.5)).toEqual("€4.50");
+  });
 });
