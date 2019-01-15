99406d86203508fe674dc0d5fb82d0d48982b76d
Cian O'Leary <cian.oleary@go1.com>
2019-01-15 11:45:11 +1000

    CourseCard Skeleton

--- src/components/CourseSlat/Skeleton/index.tsx
+++ src/components/CourseSlat/Skeleton/index.tsx
@@ -4,56 +4,50 @@ import Theme from "../../Theme";
 
 export default () => (
   <Theme.Consumer>
-    {({ spacing }) => (
+    {({ spacing, breakpoints }) => (
       <View
         borderRadius={2}
         boxShadow="crisp"
+        flexDirection="row"
+        marginBottom={4}
         color="default"
-        height="100%"
-        css={[
-          {
-            textDecoration: "none",
-            width: 221,
-          },
-        ]}
+        backgroundColor="background"
+        width="100%"
+        overflow="hidden"
       >
         <View
           padding={3}
-          alignItems="start"
           css={{
-            borderRadius: `${spacing[2]}px ${spacing[2]}px 0 0`,
-            backgroundColor: "#A6C8D0",
-            height: 128,
+            height: 142,
             width: 221,
+            backgroundColor: "#A6C8D0",
+            [breakpoints.sm]: {
+              height: 130,
+              width: 130,
+            },
           }}
         />
-        <View padding={4}>
-          <View
-            flexDirection="column"
-            justifyContent="space-between"
-            paddingBottom={2}
-          >
-            <View css={{
-              backgroundColor: "#A6C8D0",
-              height: 20,
-              maxWidth: 450,
-              width: "85%",
-              marginBottom: spacing[4],
-            }} />
-            <View css={{
-              backgroundColor: "#CDE2E7",
-              height: 13,
-              maxWidth: 250,
-              width: "50%",
-              marginBottom: spacing[3],
-            }} />
-            <View css={{
-              backgroundColor: "#CDE2E7",
-              height: 13,
-              maxWidth: 215,
-              width: "45%",
-            }} />
-          </View>
+        <View padding={5} flexGrow={1}>
+          <View css={{
+            backgroundColor: "#A6C8D0",
+            height: 20,
+            maxWidth: 450,
+            width: "70%",
+            marginBottom: spacing[4],
+          }} />
+          <View css={{
+            backgroundColor: "#CDE2E7",
+            height: 13,
+            maxWidth: 250,
+            width: "50%",
+            marginBottom: spacing[3],
+          }} />
+          <View css={{
+            backgroundColor: "#CDE2E7",
+            height: 13,
+            maxWidth: 215,
+            width: "45%",
+          }} />
         </View>
       </View>
     )}
