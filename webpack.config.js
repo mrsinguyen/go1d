131670f9195d14a9479dec515971868b7fe3b52d
Sang Le <sang@go1.com>
2019-03-18 12:40:43 +0700

    Actually check diff status for GO1 workflow

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
  * It is necessary to add the ts-loader for .tsx files
  */
 module.exports = {
+    stats: "minimal",
     resolve: {
         extensions: [".js", "md", "mdx", "jsx", ".ts", ".tsx"],
     },
